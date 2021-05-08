88f18f839f7018867a4b2994fee0b44b6b521a73
Vladimir <chertenok4413@yandex.ru>
2021-05-08 22:21:59 +0400

    add slick-slaider

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-﻿
+﻿$('.single-slide').slick({
+    slidesToScroll: 3,
+    dots: false,
+    slidesToShow: 3,
+    centerMode: true,
+    centerPadding: '0px',
+    arrows: true,
+    responsive:[
+      {
+          breakpoint: 768,
+          settings: {
+              slidesToShow:2
+          }
+      },
+      {
+          breakpoint: 550,
+          settings: {
+              slidesToShow:1
+          }
+      }
+      ]
+  });
