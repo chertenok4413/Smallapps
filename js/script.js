23eeede2c2dbcdd062d09f8d0864a1a54915733d
Vladimir <chertenok4413@yandex.ru>
2021-05-17 00:03:37 +0400

    add scroll js

--- js/script.js
+++ js/script.js
@@ -24,3 +24,13 @@
 document.getElementById("btn_clear").onclick = function(e) {
     document.getElementById("text_btn").value = "";
 };
+
+var buttons = document.querySelectorAll(".heading__one");
+
+	for (var button of buttons) {
+	   button.addEventListener('click', function () {
+	     buttons.forEach(i => i.classList.remove('active__one'));
+	   
+	     this.classList.toggle('active__one');
+	   });
+	};
