5bf98ee68b4f07125aa65d49afb658adaa9defbd
Vladimir <chertenok4413@yandex.ru>
2021-05-17 23:11:12 +0400

    add nav js

--- js/script.js
+++ js/script.js
@@ -21,6 +21,14 @@
       ]
   });
 
+var navs = document.querySelectorAll(".item");
+    for (var nav of navs) {
+        nav.addEventListener('click', function () {
+            navs.forEach(i => i.classList.remove('active'));
+            this.classList.toggle('active');
+        })
+    }
+
 document.getElementById("btn_clear").onclick = function(e) {
     document.getElementById("text_btn").value = "";
 };
